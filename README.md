0363e905817156124f1c698599a3c2821905b40d
SUNIL KUMAR YADAV <codersunilkumar@gmail.com>
2023-07-07 19:05:09 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### A Software Development Service App Using ReactJS
+
+This project is a software development service app built using ReactJS. The app is designed to be user-friendly and easy to use. 
+
+It is built using ReactJS, a popular and well-supported library for building user interfaces.
+
+### Demo link
+https://service-site.vercel.app/
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>SunielSoft</h1>
+        <p>@all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href='https://github.com/sunielkumar' target={'blank'}>
+            GitHub
+          </a>
+          <a
+            href='https://www.linkedin.com/in/sunil-kumar-yadav-a7ab43214/'
+            target={'blank'}
+          >
+            LinkedIn
+          </a>
+          <a href='https://instagram.com/__suniel12?igshid=ZDdkNTZiNTM=' target={'blank'}>
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
